94643d70ddc6223bee206f4748f32a354294810c
xumingtao <xumingtao@hipad.hk>
2014-11-25 15:01:42 +0800

    [06_python][02_tools]添加svn_del_add脚本，处理未加入svn仓库管理的文件，？文件添加，！文件删除

---
+++ 06_python/02_tools/svn_del_add.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import commands
+import sys
+import string
+import urllib
+
+is_effective = True #True False
+#0 is changelist filename.
+IGNORE_FILENAME = ('changeList.txt', 'gen_patch_directory.py', 'gen_pac.sh')
+
+def get_output(cmd):
+    system = sys.platform
+    if system == 'win32':
+        #windowns
+        r = os.popen(cmd)
+        output = r.read()
+        r.close()
+    else:
+        #linux
+        status, output = commands.getstatusoutput(cmd)
+        #print status
+    return output
+                
+def svn_add_files(add_files_list):
+    blankStr = ' '
+    svn_cmd = 'svn add --force --no-ignore %s' % blankStr.join(add_files_list)
+    commands.getoutput(svn_cmd)
+    
+def svn_delete_files(delete_files_list):
+    blankStr = ' '
+    svn_cmd = 'svn delete --force %s' % blankStr.join(delete_files_list)
+    commands.getoutput(svn_cmd)
+
+def gen_changeListFile(changeList):
+    toast = '#################changelist#################\r\n'
+    # decode the URL that contains Chinese characters
+    de = urllib.unquote
+    changeList = de(changeList)
+    # print changeList
+    changeList = changeList.replace('\n', '\r\n') + '\r\n'
+    readme = toast + changeList + toast
+    cmd = 'echo "%s" > %s' % (readme, IGNORE_FILENAME[0])
+    # print cmd
+    os.system(cmd)
+
+def gen_svn_status_list():
+    svn_cmd = 'svn st --no-ignore'
+    output = get_output(svn_cmd)
+    gen_changeListFile(output)    
+    changeList = output.splitlines()
+            
+    add_files_list = []
+    delete_files_list = []
+    ignoreFilesList = getIgnoreFilesList()
+    
+    for item in changeList:
+        #print item
+        fileList = item.split(' ')
+
+        #step 1: is it need to ignore?
+        ignore = False
+        for ignoreFileName in ignoreFilesList:
+            if ignoreFileName == fileList[-1]:
+                ignore = True
+                break
+        
+        #step 2: add this file to list
+        if not ignore:
+            if fileList[0] == '?' or fileList[0] == 'I': 
+                add_files_list.append(fileList[-1])
+            elif fileList[0] == '!':
+                delete_files_list.append(fileList[-1])
+    
+    return [add_files_list, delete_files_list]
+        
+def getIgnoreFilesList():
+    realpath = os.path.realpath(__file__)
+    currentPyFileName = os.path.basename(realpath)
+    #print currentPyFileName
+    
+    ignoreFilesList = list(IGNORE_FILENAME)
+    ignoreFilesList.append(currentPyFileName)
+    return ignoreFilesList
+
+def main():
+    print '########BEGINNING...########'
+    print '###01_gen_svn_status_list###'
+    #0 is add files, 1 is delete files
+    add_delete_files_list = gen_svn_status_list()
+    
+    print '###02_svn_add_files#########'
+    svn_add_files(add_delete_files_list[0])
+
+    print '###03_svn_delete_files######'
+    svn_delete_files(add_delete_files_list[1])
+    print '############OK!!!###########'
+
+if __name__=='__main__':
+    main()
+
+
