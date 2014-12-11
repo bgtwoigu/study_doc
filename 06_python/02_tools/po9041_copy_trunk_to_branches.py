b9ad40c5c8d74c6622c51ba1afba2dca6f5db72f
xumingtao <xumingtao@hipad.hk>
2014-12-11 11:14:56 +0800

    [06_python][tools]添加注释，去掉临时的endcommitid，每次从trunk中获取最新的id

--- 06_python/02_tools/po9041_copy_trunk_to_branches.py
+++ 06_python/02_tools/po9041_copy_trunk_to_branches.py
@@ -64,6 +64,8 @@ def rebase_trunk():
     do_git_cmd(TRUNK_PATH, git_cmd)
    
 def dcommit_branch():
+    git_cmd = 'git svn rebase'
+    do_git_cmd(BRANCH_PATH, git_cmd)
     git_cmd = 'git svn dcommit'
     do_git_cmd(BRANCH_PATH, git_cmd)
 
@@ -135,14 +137,18 @@ def gen_backup_dir(begin, end):
 def main():
     save_log('\r\n**********Begin backup all log...!********')
 
+    #step 1. get begin commit id
     begin_commit_id = get_begin_commit_id()
     if begin_commit_id == None:
         exit_with_log('begin commit id is none, exit!')
 
+    #step 2: trunk code rebase from svn repository
     rebase_trunk()
 
-    end_commit_id = '02b06284db95580573e3debfbea72a84817e5da0'
-    git_log_info = get_git_log_info(TRUNK_PATH, begin_commit_id, end_commit_id)
+    #step 3: get git log info from trunk
+    #end_commit_id = '02b06284db95580573e3debfbea72a84817e5da0'
+    #git_log_info = get_git_log_info(TRUNK_PATH, begin_commit_id, end_commit_id)
+    git_log_info = get_git_log_info(TRUNK_PATH, begin_commit_id)
     git_log_infoList = git_log_info.splitlines()
     if len(git_log_infoList) <= 0:
         exit_with_log('Thers is no git log info!It is already sync!')
@@ -151,6 +157,7 @@ def main():
     #if begin_commit_id == last_commit_id:
     #    exit_with_log('It is already sync, bengin_commit_id = last_commit_id = ' + last_commit_id)
 
+    #step 4: generate backup directory, save patch files and log info list
     backup_dir = gen_backup_dir(begin_commit_id[0:4], git_log_infoList[-1][0:4])
     save_git_log_file(backup_dir, git_log_info)
 
@@ -162,13 +169,22 @@ def main():
         #print info
         #print 'commit: ' + commit_id
         #print 'author: ' + author
+
+        #step 6: generate git format patch from trunk.
         filepath = gen_git_format_patch(TRUNK_PATH, commit_id, backup_dir)
         #print filepath
+
+        #step 7: add commit author to subject in patch
         replaceInFile(filepath, author)
+
+        #step 8: apply this format patch to branch
         if apply_git_format_patch(BRANCH_PATH, filepath):
             exit_with_log('branch git am abort! please fix it!')
-        
+    
+    #step 9: save last commit id in file.
     save_commitid_in_file(commit_id)
+
+    #step 10: dcommit all patch to svn repository.
     dcommit_branch()
 
     save_log('*********Backup end!!!**********\r\n')
